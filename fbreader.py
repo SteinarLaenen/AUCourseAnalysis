17b874af72ddb9e4d57029943cd4db1a0ae02fde
paulodder <paul_lodder@live.nl>
2017-12-07 13:08:33 +0100

    Add files

--- fbreader.py
+++ fbreader.py
@@ -1,14 +1,18 @@
-# Reader that, upon being called reads all data and writes it to a file in json
-# format (Runs in python2)
-import sys
-import facebook
+# Loads data saved to feed.txt in json format
+import json
 
-
-if __name__ == '__main__':
-    token = sys.argv[1] # token provided in command line
-    graph = facebook.GraphAPI(access_token=token)
-    target_group = graph.search(type="group", q="The excellent and diverse people of AUC")
-    group_id = target_group[u'data'][0][u'id']
-    feed = graph.get_all_connections(id=group_id, connection_name='feed')
+def split_gen(st):
+    """Takes file contents, splits based on elements in the list (i.e. json
+    formatted posts (dic with message, id, and other relevant post details)"""
+    st = st[1:] # Skip opening of list to prevent error
+    while True:
+        try:
+            cur_ind = st.index(u'},') # Corresponds to end of a post in the list
+            yield json.loads(st[:cur_ind + 1]) # Yield part corresponding to this index
+            st = st[cur_ind+2:]
+        except ValueError:
+            cur_ind = st.index(u'}]')
+            yield json.loads(st[:cur_ind+1])
+            break # reached end of list
     
-    
+            
