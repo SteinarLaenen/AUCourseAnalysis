32b143de587fc5915c79f2f3ac5ebf52b7f868b9
SteinarLaenen <steinar.laenen@student.auc.nl>
2017-12-18 10:19:34 +0100

    Added comments

--- scripts/PostFilter.py
+++ scripts/PostFilter.py
@@ -18,23 +18,27 @@ question_posts = set()
 counter = 0
 
 for i in Post.objects.all():
+	# Check if coursename + word thoughts is in text, add that post
 	for j in courselist:
 		if j in i.text and "thoughts" in i.text:
-
 			question_posts.update([i.fb_id])
+
 		if j.lower() in i.text and "thoughts" in i.text:
 			question_posts.update([i.fb_id])
 
+	#Adds some non-relevant posts
+	# most posts start with thoughts on
 	if "Thoughts on" in i.text or "thoughts on" in i.text:
 		question_posts.update([i.fb_id])
 
+	# If post contains word workload add it
 	if "workload" in i.text or "Workload" in i.text:
 		question_posts.update([i.fb_id])
 
-
+# Check if post asks about course
 for i in question_posts:
 	for j in Post.objects.filter(fb_id = i):
-		print j.text + "\n"
+		print i + "\n" + j.text + "\n"
 
 print "Number of posts:", len(question_posts)
 
