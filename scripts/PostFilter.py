0c3a5ee5f357a6a2038bba21f12d685c26086ad2
paulodder <paul_lodder@live.nl>
2017-12-19 19:38:32 +0100

    Make Steinars script a django script and set 'request' attribute to True for relvenat posts

--- scripts/PostFilter.py
+++ scripts/PostFilter.py
@@ -19,36 +19,39 @@ courselist = stringcourselist()
 question_posts = set()
 counter = 0
 
+def run():
+    for i in Post.objects.all():
+            # Check if coursename + word thoughts is in text, add that post
+            for j in courselist:
+                    if j in i.text and "thoughts" in i.text:
+                    #if re.search(j, i.text):
+                            question_posts.update([i.fb_id])
+                            i.request = True
+                            i.save()
+                    if j.lower() in i.text and "thoughts" in i.text:
+                            question_posts.update([i.fb_id])
+                            i.request = True
+                            i.save()
+            #Adds some non-relevant posts
+            # most posts start with thoughts on
+            #if "Thoughts on" in i.text or "thoughts on" in i.text:
+            #	question_posts.update([i.fb_id])
 
-for i in Post.objects.all():
-	# Check if coursename + word thoughts is in text, add that post
-	for j in courselist:
-		if j in i.text and "thoughts" in i.text:
-		#if re.search(j, i.text):
-			question_posts.update([i.fb_id])
+            # If post contains word workload add it
+            #if "workload" in i.text or "Workload" in i.text:
+            #	question_posts.update([i.fb_id])
 
-		if j.lower() in i.text and "thoughts" in i.text:
-			question_posts.update([i.fb_id])
+    # Check if post asks about course
+    for i in question_posts:
+            for j in Post.objects.filter(fb_id = i):
+                    print i + "\n" + j.text + "\n"
 
-	#Adds some non-relevant posts
-	# most posts start with thoughts on
-	#if "Thoughts on" in i.text or "thoughts on" in i.text:
-	#	question_posts.update([i.fb_id])
-
-	# If post contains word workload add it
-	#if "workload" in i.text or "Workload" in i.text:
-	#	question_posts.update([i.fb_id])
-
-# Check if post asks about course
-for i in question_posts:
-	for j in Post.objects.filter(fb_id = i):
-		print i + "\n" + j.text + "\n"
-
-print "Number of posts:", len(question_posts)
+    print "Number of posts:", len(question_posts)
 
 
-import csv
 
 
-with open("filtered_posts_fbid.txt", "wb") as outfile:
-	outfile.write(json.dumps(list(question_posts)))
+    with open("filtered_posts_fbid.txt", "wb") as outfile:
+            outfile.write(json.dumps(list(question_posts)))
+
+            
