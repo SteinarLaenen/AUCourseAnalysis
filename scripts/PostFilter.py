cb30662240616f584cddc3c1039dd4f8a20b15e7
SteinarLaenen <steinar.laenen@student.auc.nl>
2017-12-19 13:01:24 +0100

    Put filtered posts in list and text file
    hiero Paul

--- scripts/PostFilter.py
+++ scripts/PostFilter.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 
 from reviews.models import *
+import re
+import json
 
 def stringcourselist():
 	stringcourse_list = []
@@ -17,10 +19,12 @@ courselist = stringcourselist()
 question_posts = set()
 counter = 0
 
+
 for i in Post.objects.all():
 	# Check if coursename + word thoughts is in text, add that post
 	for j in courselist:
 		if j in i.text and "thoughts" in i.text:
+		#if re.search(j, i.text):
 			question_posts.update([i.fb_id])
 
 		if j.lower() in i.text and "thoughts" in i.text:
@@ -28,12 +32,12 @@ for i in Post.objects.all():
 
 	#Adds some non-relevant posts
 	# most posts start with thoughts on
-	if "Thoughts on" in i.text or "thoughts on" in i.text:
-		question_posts.update([i.fb_id])
+	#if "Thoughts on" in i.text or "thoughts on" in i.text:
+	#	question_posts.update([i.fb_id])
 
 	# If post contains word workload add it
-	if "workload" in i.text or "Workload" in i.text:
-		question_posts.update([i.fb_id])
+	#if "workload" in i.text or "Workload" in i.text:
+	#	question_posts.update([i.fb_id])
 
 # Check if post asks about course
 for i in question_posts:
@@ -43,4 +47,8 @@ for i in question_posts:
 print "Number of posts:", len(question_posts)
 
 
+import csv
 
+
+with open("filtered_posts_fbid.txt", "wb") as outfile:
+	outfile.write(json.dumps(list(question_posts)))
