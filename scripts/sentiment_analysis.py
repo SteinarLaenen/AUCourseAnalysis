8d82d310d3f59a4b6e849e5f5e97ca392a2be6d0
SteinarLaenen <steinar.laenen@student.auc.nl>
2017-12-19 14:57:27 +0100

    bugfix

--- scripts/sentiment_analysis.py
+++ scripts/sentiment_analysis.py
@@ -7,7 +7,6 @@ from reviews.models import *
 import re
 import json
 
-import pickle
 import nltk
 import sklearn
 import numpy as np
@@ -16,7 +15,8 @@ import matplotlib.pyplot as plt
 from nltk.corpus import wordnet as wn
 from nltk.corpus import sentiwordnet as swn
 
-
+un2wn_mapping = {"VERB" : wn.VERB, "NOUN" : wn.NOUN, "ADJ" : wn.ADJ, "ADV" : wn.ADV}
+stopwords = set(nltk.corpus.stopwords.words('english'))
 # Pos tags, lemmatizeds a comment accourding to Gianluca's format
 def preprocesscomment(raw_comment):
 	'''
@@ -26,9 +26,8 @@ def preprocesscomment(raw_comment):
 
 	To add: special lemmatizer for facebook?
 	'''
-	un2wn_mapping = {"VERB" : wn.VERB, "NOUN" : wn.NOUN, "ADJ" : wn.ADJ, "ADV" : wn.ADV}
-	stopwords = set(nltk.corpus.stopwords.words('english'))
-	lemmatizer = WordNetLemmatizer()
+	
+	lemmatizer = nltk.WordNetLemmatizer()
 	doc = []
 	tokenized_comment = nltk.word_tokenize(raw_comment)
 	for w, p in nltk.pos_tag(tokenized_comment, tagset="universal"):
@@ -41,9 +40,9 @@ def preprocesscomment(raw_comment):
                 else:  # filtering stopwords
                     continue
             elif un2wn_mapping.has_key(p):
-                lemma = nltk.lemmatizer.lemmatize(w.lower(), pos = un2wn_mapping[p])
+                lemma = lemmatizer.lemmatize(w.lower(), pos = un2wn_mapping[p])
             else:
-                lemma = nltk.lemmatizer.lemmatize(w.lower())
+                lemma = lemmatizer.lemmatize(w.lower())
                 
             doc.append("-".join([lemma, p]))
 	return doc
@@ -52,7 +51,7 @@ def preprocesscomment(raw_comment):
 def sentiwordnetanalysis(doc):
 	'''
 	lemmatized, post_tagged tokenized list as input in format ["word-POS"] as above.
-	returns the polarity score of this text.
+	returns the polarity score of this doc.
 	'''
 	polarity = 0
 	positive = 0
@@ -63,7 +62,8 @@ def sentiwordnetanalysis(doc):
 				positive += swn.senti_synsets(w.split("-")[0], pos = un2wn_mapping[w.split("-")[-1]])[0].pos_score()
 				negative += swn.senti_synsets(w.split("-")[0], pos = un2wn_mapping[w.split("-")[-1]])[0].neg_score()
 	polarity = positive - negative
-
+	if polarity <= 0:
+		print doc
 	return polarity
 
 
@@ -80,3 +80,6 @@ print question_posts
 for i in question_posts:
 	for j in Post.objects.filter(fb_id = i):
 		print sentiwordnetanalysis(preprocesscomment(j.text))
+
+
+print "bad", sentiwordnetanalysis(["", ""])
